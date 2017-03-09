44fc5367bb53c045ac8ce1ae2a38ceb01cae71ae
Hewon Jeong <hewon.jng@gmail.com>
2017-03-09 13:55:26 +0900

    add test code file

---
+++ test/com/hewonjeong/datastructures/NodeTest.java
@@ -0,0 +1,29 @@
+package com.hewonjeong.datastructures;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Hewon Jeong on 07/03/2017.
+ */
+class NodeTest {
+    @Test
+    public void getData() {
+
+    }
+
+    @Test
+    public void setNext() {
+
+    }
+
+    @Test
+    public void setData() {
+
+    }
+
+    @Test
+    public String toString() {
+        return null;
+    }
+
+}
