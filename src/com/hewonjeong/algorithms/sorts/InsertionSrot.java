0baf81ebcc2d391504dcdbbd8becc90cd02decf3
Hewon Jeong <hewon.jng@gmail.com>
2017-03-27 17:46:09 +0900

    Add InsertionSort class

---
+++ src/com/hewonjeong/algorithms/sorts/InsertionSrot.java
@@ -0,0 +1,19 @@
+package com.hewonjeong.algorithms.sorts;
+
+/**
+ * Created by Hewon Jeong on 27/03/2017.
+ */
+public class InsertionSrot {
+    public static <T extends Comparable<T>> T[] sort(T[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            T target = arr[i];
+            int pointer = i;
+            while (pointer > 0 && target.compareTo(arr[pointer - 1]) < 0) {
+                arr[pointer] = arr[pointer - 1];
+                pointer--;
+            }
+            arr[pointer] = target;
+        }
+        return arr;
+    }
+}
